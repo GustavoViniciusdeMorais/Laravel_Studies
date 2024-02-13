91df67ae746c0010e5c024791b6879e5d47c58e7
gustavomoraispereira <gustavo.pereira@dotgroup.com.br>
2024-02-13 13:40:36 -0300

    - fixed package route endpoint to controller bug

--- api/modules/article/src/routes/web.php
+++ api/modules/article/src/routes/web.php
@@ -12,12 +12,12 @@ Route::get('api/articles/check', function () {
     ]);
 });
 
-// Route::get('api/articles', function () {
-//     return new JsonResponse([
-//         'status' => 'success',
-//         'data' => true
-//     ]);
-// });
-
-// Route::get('/api/articles', 'GustavoMorais\Article\Infrastructure\Controllers\ArticleController@listArticles');
-Route::get('/api/articles', [ArticleController::class, 'listArticles']);
+Route::group(
+    [
+        'prefix' => 'api',
+        'namespace' => 'GustavoMorais\Article\Infrastructure\Controllers'
+    ],
+    function () {
+        Route::get('articles', ['uses' => 'ArticleController@listArticles']);
+    }
+);
