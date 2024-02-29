c61082091d254db9ff9640907a6a7a32e38e98a8
gustavomoraispereira <gustavo.pereira@dotgroup.com.br>
2024-02-29 13:52:36 -0300

    - Improved graphql article endpoint controller for resolver

--- api/modules/article/src/Infrastructure/Controllers/ArticleController.php
+++ api/modules/article/src/Infrastructure/Controllers/ArticleController.php
@@ -7,6 +7,8 @@ use GustavoMorais\Article\Application\Commands\CreateArticleAction;
 use GustavoMorais\Article\Infrastructure\Controllers\BaseController;
 use Illuminate\Http\Request;
 use GustavoMorais\Article\Infrastructure\Graphql\ArticleResolver;
+use GraphQL\Executor\Executor;
+use GraphQL\Type\Definition\ResolveInfo;
 
 class ArticleController extends BaseController
 {
@@ -54,42 +56,26 @@ class ArticleController extends BaseController
 
     private function setResolvers($resolvers)
     {
-        \GraphQL\Executor\Executor::setDefaultFieldResolver(
-            function ($source, $args, $context, \GraphQL\Type\Definition\ResolveInfo $info) use ($resolvers) {
+        Executor::setDefaultFieldResolver(
+            function ($source, $args, $context, ResolveInfo $info) use ($resolvers) {
                 $fieldName = $info->fieldName;
-                if (is_null($fieldName)) {
-                    throw new \Exception('Could not get $fieldName from ResolveInfo');
-                }
-                if (is_null($info->parentType)) {
-                    throw new \Exception('Could not get $parentType from ResolveInfo');
+                if (is_null($fieldName) || is_null($info->parentType)) {
+                    throw new \Exception('Could not get $fieldName or $parentType from ResolveInfo');
                 }
                 $parentTypeName = $info->parentType->name;
                 if (isset($resolvers[$parentTypeName])) {
                     $resolver = $resolvers[$parentTypeName];
-                    if (is_array($resolver)) {
-                        if (array_key_exists($fieldName, $resolver)) {
-                            $value = $resolver[$fieldName];
-                            return is_callable($value) ? $value(
-                                $source,
-                                $args,
-                                $context,
-                                $info
-                            ) : $value;
-                        }
-                    }
-                    if (is_object($resolver)) {
-                        if (isset($resolver->{$fieldName})) {
-                            $value = $resolver->{$fieldName};
-                            return is_callable($value) ? $value(
-                                $source,
-                                $args,
-                                $context,
-                                $info
-                            ) : $value;
-                        }
+                    if (is_array($resolver) && array_key_exists($fieldName, $resolver)) {
+                        $value = $resolver[$fieldName];
+                        return is_callable($value) ? $value(
+                            $source,
+                            $args,
+                            $context,
+                            $info
+                        ) : $value;
                     }
                 }
-                return \GraphQL\Executor\Executor::defaultFieldResolver($source, $args, $context, $info);
+                return Executor::defaultFieldResolver($source, $args, $context, $info);
         });
     }
 }
