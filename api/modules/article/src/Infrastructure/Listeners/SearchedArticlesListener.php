4683f7f2fe4adbc273499b9085fd8a6f51ca015f
gustavomoraispereira <gustavo.pereira@dotgroup.com.br>
2024-03-07 14:42:08 -0300

    - added queue message producer inside listener, but still not working

--- api/modules/article/src/Infrastructure/Listeners/SearchedArticlesListener.php
+++ api/modules/article/src/Infrastructure/Listeners/SearchedArticlesListener.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use GustavoMorais\Article\Infrastructure\Events\SearchedArticlesEvent;
 use Illuminate\Support\Facades\Log;
+use GustavoMorais\Article\Infrastructure\Queue\Producer;
 
 class SearchedArticlesListener
 {
@@ -27,6 +28,8 @@ class SearchedArticlesListener
      */
     public function handle(SearchedArticlesEvent $event)
     {
+        $producer = new Producer();
+        $producer->produce("test@email.com", "Article reader");
         Log::info('SearchedArticlesEvent');
         print_r(json_encode(['searched' => $event->data]));echo "\n\n";exit;
     }
